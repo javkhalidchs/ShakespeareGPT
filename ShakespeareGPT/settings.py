7669a71d2af2e00f366b4c7c0683448c56244603
ZulqarnainAhmed123 <zulqarnainahmed22@yahoo.com>
2023-05-04 11:19:17 +0500

    change template to /tmp/8db4c664436bb17/templates

--- ShakespeareGPT/settings.py
+++ ShakespeareGPT/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ShakespeareGPT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / '/tmp/8db4c664436bb17/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
